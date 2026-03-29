e63a4504da0aca7e31041846fa2eee349ebdea70
mighty808 <effahaboagye888@gmail.com>
2026-03-29 14:33:35 +0000

    Update app bootstrap

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./lib/auth-context";
+import { AdminAuthProvider } from "./lib/admin-auth-context";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AdminAuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AdminAuthProvider>
     </AuthProvider>
   </StrictMode>,
 );
