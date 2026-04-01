bf595cded64371491df17d763d3cabf872fbd45e
mighty808 <effahaboagye888@gmail.com>
2026-04-01 00:10:22 +0000

    chore: remove admin provider from client

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -4,16 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./lib/auth-context";
-import { AdminAuthProvider } from "./lib/admin-auth-context";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <AdminAuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AdminAuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
 );
